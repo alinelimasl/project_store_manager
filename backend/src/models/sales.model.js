de60dac0b081eb1b8015e859d89f4bd276db22ac
Aline <aline_lima@outlook.com>
2023-11-28 09:13:15 -0300

    Correção de erros mas falta testes

--- backend/src/models/sales.model.js
+++ backend/src/models/sales.model.js
@@ -2,7 +2,7 @@ const camelize = require('camelize');
 const connection = require('./connection');
 
 const getAllSales = async () => {
-  const results = await connection.execute(
+  const [results] = await connection.execute(
     `SELECT sap.sale_id, sa.date, sap.product_id, sap.quantity FROM sales AS sa 
      INNER JOIN sales_products AS sap ON sap.sale_id = sa.id 
      ORDER BY sap.sale_id, sap.product_id;`,
